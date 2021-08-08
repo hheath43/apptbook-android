c6949420a409977bf5116770aaf6137839705925
Hannah Heath <heathhan@pdx.edu>
2021-08-08 15:53:53 -0700

    Added onClick for viewing all appointments now PrettyPrinted

---
+++ app/src/main/java/edu/pdx/cs410J/heathhan/PrettyPrinter.java
@@ -0,0 +1,99 @@
+package edu.pdx.cs410J.heathhan;
+
+import edu.pdx.cs410J.AppointmentBookDumper;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * Class represents an <code>AppointmentBook</code>
+ */
+public class PrettyPrinter implements AppointmentBookDumper<AppointmentBook> {
+
+    private Writer writer;
+
+    /**
+     * Creates a new <code>PrettyPrinter</code> For StdOut
+     */
+    public PrettyPrinter(){
+
+    }
+
+    /**
+     * Creates a new <code>PrettyPrinter</code> For file
+     *
+     * @param writer - Writer
+     *               File to write the
+     */
+    public PrettyPrinter(Writer writer) {
+        this.writer = writer;
+    }
+
+
+    /**
+     * Method to dump an appointment book to a file nicely.
+     *
+     * @param book
+     *      Appointment book to be pretty printed to a file.
+     *
+     * @throws IOException
+     *          IOException if the file is unreachable.
+     */
+    public void dump(AppointmentBook book) throws IOException {
+        try {
+            writer.write("Appointment Book Owner: " + book.getOwnerName() + "\n\n");
+            Collection<Appointment> s = book.getAppointments();
+            long duration;
+
+            if(s.isEmpty()) {
+                writer.write("No Appointments");
+                writer.flush();
+            } else {
+
+                for (Appointment appt : s) {
+                    writer.write("Appointment Description: " + appt.getDescription() + "\n");
+                    writer.write("Begins: " + appt.getBeginTimeString() + "\n");
+                    writer.write("Ends: " + appt.getEndTimeString() + "\n");
+                    duration = Math.abs(appt.getEndTime().getTime() - appt.getBeginTime().getTime());
+                    duration = TimeUnit.MINUTES.convert(duration, TimeUnit.MILLISECONDS);
+                    writer.write("Duration: " + String.valueOf(duration) + " Minutes");
+                    writer.write("\n\n");
+                }
+
+                writer.flush();
+            }
+        } catch (IOException e) {
+            throw new IOException("File could not be found" + e);
+        }
+    }
+
+    /**
+     * Method to StandardOut an appointment book nicely.
+     *
+     * @param book
+     *      Appointment book to be pretty printed.
+     */
+    public void dumpStandardOut(AppointmentBook book){
+        System.out.println("Appointment Book Owner: " + book.getOwnerName() + "\n");
+        Collection<Appointment> apptbook = book.getAppointments();
+        long duration;
+
+        if(apptbook.isEmpty()){
+            System.out.println("No Appointments");
+        }
+        else{
+            for (Appointment appt : apptbook) {
+                System.out.println("Appointment Description: " + appt.getDescription());
+                System.out.print("Begins: " + appt.getBeginTimeString() + "\n" + "Ends: " + appt.getEndTimeString() + "\n");
+                duration = Math.abs(appt.getEndTime().getTime() - appt.getBeginTime().getTime());
+                duration = TimeUnit.MINUTES.convert(duration, TimeUnit.MILLISECONDS);
+                System.out.println("Duration: " + String.valueOf(duration) + " Minutes");
+                System.out.println("\n");
+            }
+        }
+    }
+
+}
--- app/src/main/java/edu/pdx/cs410J/heathhan/ViewAllActivity.java
+++ app/src/main/java/edu/pdx/cs410J/heathhan/ViewAllActivity.java
@@ -6,6 +6,14 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ViewAllActivity extends AppCompatActivity {
     private ArrayAdapter<Appointment> appointments;
@@ -14,15 +22,56 @@ public class ViewAllActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_all);
+        PrettyPrinter pretty;
+        BufferedReader reader;
+        StringBuilder text = new StringBuilder();
+        String line = null;
 
         Bundle b = getIntent().getExtras();
         AppointmentBook book = (AppointmentBook) b.get("appointmentBook");
+        File file = getFile(book.getOwnerName());
+
+        try {
+            pretty = new PrettyPrinter(new FileWriter(file));
+            pretty.dump(book);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+
+            while((line = reader.readLine()) != null){
+                text.append(line);
+                text.append('\n');
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
-        this.appointments = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
-        ListView listOfAppt = findViewById(R.id.appointments);
-        appointments.addAll(book.getAppointments());
-        listOfAppt.setAdapter(appointments);
+        //this.appointments = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        TextView outputOfAppts = findViewById(R.id.appointments);
+        outputOfAppts.setText((CharSequence) text);
+        //appointments.addAll(book.getAppointments());
+        //listOfAppt.setAdapter(appointments);
+    }
+
+    private File getFile(String owner){
+        String str = replaceSpace(owner);
+        str = str + "2" + ".txt";
+
+        File contextDirectory = getApplicationContext().getDataDir();
+        File file = new File(contextDirectory, str);
+
+        return file;
+    }
+
+    private String replaceSpace(String str){
+        str = str.replace(" ", "_");
+        return str;
     }
 
 }
